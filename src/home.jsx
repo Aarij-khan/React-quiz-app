f5ac0417b7e032b4532cab7c5087dca3b1635491
Aarij khan <aarij0100@gmail.com>
2024-09-21 03:55:06 +0500

    screens update

--- src/home.jsx
+++ src/home.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from 'react-router-dom';
 function Home() {
     const navigate = useNavigate()
   const [data ,setData] = useState([])
-  console.log("🚀 ~ Home ~ data:", data)
   useEffect(() => {
     
    setData(arr)
@@ -14,7 +13,7 @@ function Home() {
   return (
     <div>
       <Navbar/>
-      <div className='w-full  justify-center bg-green-400 flex flex-wrap py-20 gap-6'>
+      <div className='w-full  justify-center bg-green-500 flex flex-wrap py-20 gap-6'>
         {data.map((e,idx)=>{
           return(
         <div className='h-[200px] w-[270px] xl:w-[300px] bg-zinc-600  rounded-xl flex items-center gap-6 justify-center flex-col xl:mx-10' key={idx}>
@@ -27,7 +26,7 @@ function Home() {
           </h1>
          
             
-            <button type="button" onClick={()=>navigate('/quizpage')} class="w-[250px] focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" >Start Quiz</button>
+            <button type="button" onClick={()=>navigate('/quizpage',{state : e})} class="w-[250px] focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" >Start Quiz</button>
 
         </div>
 
--- src/quizpage.jsx
+++ src/quizpage.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Navbar from "./navbar";
 function Quizpage() {
+  const { state } = useLocation();
+  return (<>
 
-    const state = useLocation()
-    console.log("🚀 ~ Quizpage ~ state:", state)
-  return (
+        <Navbar/>
+    <div className="h-screen w-full flex justify-center items-center bg-lime-500 ">
+        <div className="max-w-sm bg-white p-10 flex flex-col gap-4">
+      <h5 className="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
+        {state.course}
+      </h5>
+      <p className=" text-xl font-bold  text-gray-700 dark:text-gray-400 text-center">
+      {state.duration}
+      </p>
+      <button type="button"  className="w-[250px] focus:outline-none text-white bg-lime-600 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" >Start Quiz</button>
 
-    <div>Quizpage</div>
-  )
+    </div>
+
+
+    </div>
+    </>
+  );
 }
 
-export default Quizpage
+export default Quizpage;
